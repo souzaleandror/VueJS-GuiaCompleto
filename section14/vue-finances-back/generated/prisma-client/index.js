3e8001309f5777f36f801d4c503c5e15944caafc
souza.leandror@gmail.com <souza.leandror@gmail.com>
2020-05-06 10:20:23 +0200

    Update 50%

---
+++ section14/vue-finances-back/generated/prisma-client/index.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Account",
+    embedded: false
+  },
+  {
+    name: "Operation",
+    embedded: false
+  },
+  {
+    name: "Category",
+    embedded: false
+  },
+  {
+    name: "Record",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466`
+});
+exports.prisma = new exports.Prisma();
